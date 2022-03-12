4e8cabe5f4a699360fbd58dd6f0a40914b5c000a
hugosenari <hugosenari@gmail.com>
2022-03-12 02:36:00 -0300

    chore: re index db_clickhouse

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''ClickHouse Nim interface'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-db_clickhouse-master.flake = false;
+  inputs.src-db_clickhouse-master.owner = "leonardoce";
+  inputs.src-db_clickhouse-master.ref   = "refs/heads/master";
+  inputs.src-db_clickhouse-master.repo  = "nim-clickhouse";
+  inputs.src-db_clickhouse-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-db_clickhouse-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-db_clickhouse-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ v0_2_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''ClickHouse Nim interface'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-db_clickhouse-v0_2_0.flake = false;
+  inputs.src-db_clickhouse-v0_2_0.owner = "leonardoce";
+  inputs.src-db_clickhouse-v0_2_0.ref   = "refs/tags/v0.2.0";
+  inputs.src-db_clickhouse-v0_2_0.repo  = "nim-clickhouse";
+  inputs.src-db_clickhouse-v0_2_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-db_clickhouse-v0_2_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-db_clickhouse-v0_2_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
